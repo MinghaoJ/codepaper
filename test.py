eca2c80a57b3d152d7756123c29ebe36cf73c7b2
Neil Parikh <parikh.neil@me.com>
2015-01-31 10:57:54 -0500

    move node and edge dectection into function

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import numpy as np
 import cv2
 
-red_lower_bound1 = np.array([0,25,90])
-red_upper_bound1 = np.array([10,255,191])
-red_lower_bound2 = np.array([170,25,90])
-red_upper_bound2 = np.array([179,255,191])
+def edge_node_mask(im):
+    red_lower_bound1 = np.array([0,25,90])
+    red_upper_bound1 = np.array([10,255,191])
+    red_lower_bound2 = np.array([170,25,90])
+    red_upper_bound2 = np.array([179,255,191])
 
-im = cv2.imread('test4.png')
-im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-edgemask = cv2.bitwise_or(cv2.inRange(im,red_lower_bound1,red_upper_bound1),cv2.inRange(im,red_lower_bound2,red_upper_bound2))
-(_,allmask) = cv2.threshold(im_grey,90,255,cv2.THRESH_BINARY_INV)
-nodemask = cv2.bitwise_and(allmask,cv2.bitwise_not(edgemask))
+    edgemask = cv2.bitwise_or(cv2.inRange(im,red_lower_bound1,red_upper_bound1),cv2.inRange(im,red_lower_bound2,red_upper_bound2))
+    (_,allmask) = cv2.threshold(im_grey,90,255,cv2.THRESH_BINARY_INV)
+    nodemask = cv2.bitwise_and(allmask,cv2.bitwise_not(edgemask))
+    return (nodemask, edgemask)
+
+(nodemask, edgemask) = edge_node_mask(cv2.imread('test4.png'))
 
 nodes, hierarchy = cv2.findContours(nodemask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(nodemask, nodes, -1, (255,100,100), 3)
