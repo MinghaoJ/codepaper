e8067cc7f93a460276003aa1f1928cdac3abd7f1
Minghao <minghao.ji@outlook.com>
2015-01-31 11:02:58 -0500

    consolidate variable naming conventions

--- test.py
+++ test.py
@@ -10,16 +10,16 @@ def edge_node_mask(im):
     im_grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
-    edgemask = cv2.bitwise_or(cv2.inRange(im,red_lower_bound1,red_upper_bound1),cv2.inRange(im,red_lower_bound2,red_upper_bound2))
+    edge_mask = cv2.bitwise_or(cv2.inRange(im,red_lower_bound1,red_upper_bound1),cv2.inRange(im,red_lower_bound2,red_upper_bound2))
     (_,allmask) = cv2.threshold(im_grey,90,255,cv2.THRESH_BINARY_INV)
-    nodemask = cv2.bitwise_and(allmask,cv2.bitwise_not(edgemask))
-    return (nodemask, edgemask)
+    node_mask = cv2.bitwise_and(allmask,cv2.bitwise_not(edge_mask))
+    return (node_mask, edge_mask)
 
-(nodemask, edgemask) = edge_node_mask(cv2.imread('test4.png'))
+(node_mask, edge_mask) = edge_node_mask(cv2.imread('test4.png'))
 
-nodes, hierarchy = cv2.findContours(nodemask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(nodemask, nodes, -1, (255,100,100), 3)
-edges, hierarchy = cv2.findContours(edgemask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+nodes, hierarchy = cv2.findContours(node_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(node_mask, nodes, -1, (255,100,100), 3)
+edges, hierarchy = cv2.findContours(edge_mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 parsed_nodes = []
 parsed_edges = []
@@ -40,12 +40,9 @@ for c in edges :
 print "Total edges:"
 print len(parsed_edges)
 
-#lines = cv2.HoughLinesP(parsed_edges,1,np.pi/180,100,100,100)
-#for x1,y1,x2,y2 in lines[0]:
-#    cv2.line(edgemask,(x1,y1),(x2,y2),(0,255,0),2)
 
-cv2.drawContours(nodemask, parsed_nodes, -1, (255,255,255), 3)
-cv2.drawContours(edgemask, parsed_edges, -1, (255,255,255), 3)
+cv2.drawContours(node_mask, parsed_nodes, -1, (255,255,255), 3)
+cv2.drawContours(edge_mask, parsed_edges, -1, (255,255,255), 3)
 	
-cv2.imwrite('output.png',edgemask) 
-cv2.imwrite('output2.png',nodemask)
+cv2.imwrite('output.png',edge_mask) 
+cv2.imwrite('output2.png',node_mask)
