c94b17b05bb1ebd58eb018d9174aaa30f499bff4
Neil Parikh <parikh.neil@me.com>
2015-01-31 22:40:06 -0500

    only ask for input for nodes that feed to multiple other node once

--- eval.py
+++ eval.py
@@ -1,5 +1,7 @@
 def evaluate(node):
     if len(node.inputs) == 0:
-        return input("Enter a value for " + node.name + " ")
+        if !node.value
+            self.value = input("Enter a value for " + node.name + " ")
+        return self.value
     else:
         return node.function(map(evaluate, node.inputs))
