3c5a495e4f2009716e89190d6e8c0ae433acdbc2
Minghao <minghao.ji@outlook.com>
2015-01-31 22:20:36 -0500

    Revert "add factorial function"
    This reverts commit f6819f3e9e55b67c47235884d19ac417f9f58d94.

--- eval.py
+++ eval.py
@@ -2,4 +2,4 @@ def evaluate(node):
     if len(node.inputs) == 0:
         return input("Enter a value for " + node.name + " ")
     else:
-        return node.function(map(evaluate, node.inputs))
+        return node.operation(map(evaluate, node.inputs))
